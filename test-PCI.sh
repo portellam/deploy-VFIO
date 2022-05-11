876918b5792c05ae139b7d2f55ad94841973fe38
Alex <amportell.13@gmail.com>
2022-05-11 22:08:33 +0000

    Update test-PCI.sh

--- test-PCI.sh
+++ test-PCI.sh
@@ -14,20 +14,21 @@ function ParsePCI {
     #
 
     # lists #
-    declare -a arr_lspci=`lspci | grep ":00."`      # Bus ID
-    declare -a arr_lspci_k=`lspci -k`               # Driver
-    declare -a arr_lspci_n=`lspci -n | grep ":00."` # HW ID
+    declare -a arr_lspci=`lspci`        # Bus ID
+    declare -a arr_lspci_k=`lspci -k`   # Driver
+    declare -a arr_lspci_m=`lspci -m`   # machine readable
+    declare -a arr_lspci_n=`lspci -n`   # HW ID
     #
-    declare -a arr_PCIBusID=""
-    declare -a arr_PCIHWID=""
-    declare -a arr_PCIDriver=""
-    declare -a arr_PCIInfo=""
-    declare -a arr_VGABusID=""
-    declare -a arr_VGADriver=""
-    declare -a arr_VGAHWID=""
-    declare -a arr_VGAVendorBusID=""
-    declare -a arr_VGAVendorDriver=""
-    declare -a arr_VGAVendorHWID=""
+    declare -a arr_PCIBusID
+    declare -a arr_PCIHWID
+    declare -a arr_PCIDriver
+    declare -a arr_PCIInfo
+    declare -a arr_VGABusID
+    declare -a arr_VGADriver
+    declare -a arr_VGAHWID
+    declare -a arr_VGAVendorBusID
+    declare -a arr_VGAVendorDriver
+    declare -a arr_VGAVendorHWID
     #
 
     # find external PCI device Index values, Bus ID, and drivers ##
@@ -37,121 +38,122 @@ function ParsePCI {
     declare -i int_index=0
     #
 
-    # parse list of PCI devices #
-    for str_line in $arr_lspci_k; do
+        # save for Xorg
+            # convert raw string to copy readable by Xorg
+            # ab:cd.e   > b:d:e
+            #str_thisPCIBusID=${str_thisPCIBusID:1:2}${str_thisPCIBusID:4:3}                                             # ab:cd.e   > b:d.e
+            #str_thisPCIBusID=$(echo $str_thisPCIBusID | cut -d '.' -f 1)":"$(echo $str_thisPCIBusID | cut -d '.' -f 2)  # b:d.e     > b:d:e
+        #
 
-        if [[ ${str_line:0:7} == "01:00.0" ]]; then bool_parsePCI=true; fi
 
-        # parse external PCI devices #
-        if [[ $bool_parsePCI == true ]]; then
+    ## NEW BELOW ##
 
-            if [[ $str_line != *"Subsystem: "* && $str_line != *"Kernel driver in use: "* && $str_line != *"Kernel modules: "* ]]; then     # line includes PCI bus ID
+    for str_line1 in $arr_lspci_m; do
 
-                ((int_index++))                     # increment only if Bus ID is found
-                arr_PCIInfo+=($str_line)            # add line to list
-                str_thisPCIBusID=(${str_line:0:7})  # raw VGA Bus ID
-                # ab:cd.e   > b:d:e
-                str_thisPCIBusID=${str_thisPCIBusID:1:2}${str_thisPCIBusID:4:3}                                             # ab:cd.e   > b:d.e
-                str_thisPCIBusID=$(echo $str_thisPCIBusID | cut -d '.' -f 1)":"$(echo $str_thisPCIBusID | cut -d '.' -f 2)  # b:d.e     > b:d:e
+        # save Info #
+        str_thisPCIBusID=(${str_line1:0:7})                     # raw Bus ID    (example AB:CD.E)
+        str_somePCIBusID=`$str_thisPCIBusID | cut -d "." -f 1`  # truncated ID  (example AB:CD)
+        echo "str_somePCIBusID == $str_somePCIBusID"
+        str_thisPCIType=`$str_line1 | cut -d '"' -f 2`          # Type          (example VGA)
+        echo "str_thisPCIType == $str_thisPCIType"
+        str_thisPCIVendor=`$str_line1 | cut -d '"' -f 4`        # Vendor name   (example NVIDIA)
+        echo "str_thisPCIVendor == $str_thisPCIVendor"
+        #
+
+        arr_PCIBusID+=("$str_thisPCIBusID")                     # add to list
+
+        # match VGA device #
+        if [[ $str_line1 == *"VGA"* ]]; then
+        
+            # convert raw string to copy readable by Xorg
+            # ab:cd.e   > b:d:e
+            str_thisVGABusID=${str_thisPCIBusID:1:2}${str_thisPCIBusID:4:3}
+            echo "str_thisVGABusID == $str_thisVGABusID"
+            # b:d.e     > b:d:e
+            str_thisVGABusID=$(echo $str_thisPCIBusID | cut -d '.' -f 1)":"$(echo $str_thisPCIBusID | cut -d '.' -f 2)
+            echo "str_thisVGABusID == $str_thisVGABusID"
+            arr_VGABusID+=("$str_thisVGABusID")
+        
+        fi
+        #
+
+        # match VGA Vendor from last line #
+        # NOTE: non VGA devices may also have multiple interfaces (?), but that is irrelevant for the objective here.
+        # Objective: list all PCI devices, and VGA and VGA Vendor devices.
+        if [[ $str_line1 != *"VGA"* && $str_prevLine1 == *"$str_thisPCIVendor"* ]]; then arr_VGAVendorBusID+=("$str_thisPCIBusID"); fi
+        #
+
+        # parse HW IDs #
+        for str_line2 in $arr_lspci_n; do
+
+            str_thisPCIHWID=(${str_line2:14:9})             # Hardware ID
+
+            # match Bus ID
+            if [[ $str_thisPCIBusID == ${str_line2:0:7} ]]; then
+
+                arr_PCIHWID+=("$str_thisPCIHWID")           # add to list
 
                 # match VGA device #
-                if [[ $str_line == *"VGA"* ]]; then
-            
-                    arr_VGABusID+=($str_thisPCIBusID)
-                    str_prevPCIBusID=($str_thisPCIBusID)
-                    bool_parseVGA=true
-
-                fi
+                if [[ $str_line1 == *"VGA"* ]]; then arr_VGAHWID+=("$str_thisPCIHWID"); fi    # add to list
                 #
 
-                # match VGA vendor device (Audio, USB3, etc.) #
-                if [[ ${str_thisPCIBusID:0:3} == ${str_prevPCIBusID:0:3} && $str_thisPCIBusID != $str_prevPCIBusID ]]; then
-
-                    arr_VGAVendorBusID+=($str_thisPCIBusID)
-                    bool_parseVGAVendor=true
-
-                fi
+                # match VGA Vendor from last line #
+                if [[ $str_line1 != *"VGA"* && $str_prevLine1 == *"$str_somePCIBusID"* && $str_prevLine1 == *"$str_thisPCIVendor"* ]]; then arr_VGAVendorHWID+=("$str_thisPCIHWID"); fi        # add to list
                 #
-
-                # no match #
-                if [[ $bool_parseVGA == false && $bool_parseVGAVendor == false ]]; then arr_PCIBusID+=($str_thisPCIBusID); fi
-                #
-
             fi
+            #
+        done
+        #
 
-            # add to list only if driver is found #
-            if [[ $str_line == *"Kernel driver in use: "* ]]; then      # line includes PCI driver
+        # parse Drivers #
+        for str_line3 in $arr_lspci_k; do
+
+            # match Bus ID
+            if [[ $str_thisPCIBusID == ${str_line3:0:7} ]]; then
+
+                arr_PCIDriver+=("$str_thisPCIDriver")                           # add to list
+
+                # match VGA device #
+                if [[ $str_line1 == *"VGA"* ]]; then bool_parseVGA=true; fi     # add to list
+                #
+
+                # match VGA Vendor from last line #
+                if [[ $str_line1 != *"VGA"* && $str_prevLine1 == *"$str_somePCIBusID"* && $str_prevLine1 == *"$str_thisPCIVendor"* ]]; then bool_parseVGAVendor=true; fi         # add to list
+                #  
+            fi
+            #
+
+            # before next Bus ID, match Driver
+            if [[ $str_line3 == *"Kernel driver in use: "* ]]; then
 
                 declare -i int_offset=${#str_line}-22
                 str_thisPCIDriver=${str_line:23:$int_offset}
+                arr_PCIDriver+=("$str_thisPCIDriver")               # add to list
 
                 # match VGA device #
-                if [[ $bool_parseVGA == true ]]; then arr_VGADriver+=($str_thisPCIDriver); fi
+                if [[ $bool_parseVGA == true ]]; then
+                    arr_VGADriver+=("$str_thisPCIDriver")           # add to list
+                    bool_parseVGA=false
+                fi
                 #
 
-                # match VGA vendor device #
-                if [[ $bool_parseVGAVendor == true ]]; then arr_VGAVendorDriver+=("$str_thisPCIDriver"); fi
+                # match VGA Vendor from last line #
+                if [[ $bool_parseVGAVendor == true ]]; then
+                    arr_VGAVendorDriver+=("$str_thisPCIDriver")     # add to list
+                    bool_parseVGAVendor=false
+                fi
                 #
-
-                # no match #
-                if [[ $bool_parseVGA == false && $bool_parseVGAVendor == false ]]; then arr_PCIDriver+=($str_thisPCIDriver); fi
-                #
-
-                bool_parseVGA=false
-                bool_parseVGAVendor=false
-
             fi
             #
-
-        fi
+        done
         #
+
+        str_prevLine1=$str_line1    # save previous line for comparison
 
     done
     #
 
-    # find external PCI Hardware IDs #
-    bool_parseVGAVendor=false
-    declare -i int_index=0
-
-    # parse list of PCI devices #
-    for str_line in $arr_lspci_n; do
-
-        if [[ ${str_line:0:7} == "01:00.0" ]]; then bool_parsePCI=true; fi
-
-        # parse external PCI devices ##
-        if [[ $bool_parsePCI == true ]]; then 
-
-            arr_PCIInfo+=($str_line)            # add line to list
-            str_thisPCIBusID=(${str_line:0:7})  # raw VGA Bus ID
-            # ab:cd.e   > b:d:e
-            str_thisPCIBusID=${str_thisPCIBusID:1:2}${str_thisPCIBusID:4:3}                                            # ab:cd.e   > b:d.e
-            str_thisPCIBusID=$(echo $str_thisPCIBusID | cut -d '.' -f 1)":"$(echo $str_thisPCIBusID | cut -d '.' -f 2)  # b:d.e     > b:d:e
-            str_thisPCIHWID=(${str_line:14:9})
-
-            # add to list only if VGA device #
-            if [[ ${arr_PCIInfo[$int_index]} == *"VGA"* ]]; then
-            
-                arr_VGAHWID+=($str_thisPCIHWID)
-                str_prevPCIBusID=($str_thisPCIBusID)
-                
-            fi
-            #
-
-            # add to lists only if VGA vendor device #
-            if [[ ${str_thisPCIBusID:0:3} == ${str_prevPCIBusID:0:3} && $str_thisPCIBusID != $str_prevPCIBusID ]]; then arr_VGAVendorHWID+=($str_thisPCIHWID); fi
-            #
-
-            # add to list #
-            if [[ ${arr_PCIInfo[$int_index]} != *"VGA"* && ${str_thisPCIBusID:0:3} != ${str_prevPCIBusID:0:3} ]]; then arr_PCIHWID+=($str_thisPCIHWID); fi
-            #
-
-            ((int_index++))     # increment only if match is found
-
-        fi
-        #
-
-    done
-    #
+    ## NEW ABOVE ##
 
     # debug #
     function ParsePCIDebug {
@@ -205,7 +207,7 @@ function ParsePCI {
     #
 
     # debug #
-    ParsePCIDebug
+    #ParsePCIDebug
     #
 
 }
