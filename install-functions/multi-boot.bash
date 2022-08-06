092042f9f23fcd144a5cb3a58ccd607ec2dcecfd
Alex Portell <amportell.13@gmail.com>
2022-08-06 06:51:59 -0400

    Delete multi-boot.bash

--- install-functions/multi-boot.bash
+++
@@ -1,6 +0,0 @@
-
-declare -a arr_rootKernel+=(`ls -1 /boot/vmli* | cut -d "z" -f 2`)
-
-for str_rootKernel in ${arr_rootKernel[@]}; do
-    str_output1="+'`lsb_release -i -s` `uname -o`, with `uname` $str_rootKernel (VGA: $str_thisVGA_deviceName)" 
-done
