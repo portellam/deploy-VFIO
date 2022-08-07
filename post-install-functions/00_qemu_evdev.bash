456d4864632729786f3d56468fa63d8f52f02f19
Alex Portell <amportell.13@gmail.com>
2022-08-07 11:01:10 -0400

    evdev fix

--- post-install-functions/00_qemu_evdev.bash
+++ post-install-functions/00_qemu_evdev.bash
@@ -23,13 +23,15 @@ bool_missingFiles=false
 str_outFile1="/etc/libvirt/qemu.conf"
 
 # input files #
-str_inFile1=`find . -name *etc_libvirt_qemu.conf*`
+str_inFile1=`../files/*etc_libvirt_qemu.conf*`
+echo $str_inFile1
 
 # system file backups #
 str_oldFile1=$str_outFile1"_old"
 
 # debug logfiles #
-str_logFile0=`find . -name *hugepages*log*`
+str_logFile0=`../files/*hugepages*log*`
+echo $str_logFile0
 
 # prompt #
 str_output1="$0: Evdev (Event Devices) is a method that assigns input devices to a Virtual KVM (Keyboard-Video-Mouse) switch.\n\tEvdev is recommended for setups without an external KVM switch and passed-through USB controller(s).\n\tNOTE: View '/etc/libvirt/qemu.conf' to review changes, and append to a Virtual machine's configuration file."
@@ -46,20 +48,20 @@ for str_User in $arr_User; do
     sudo adduser $str_User libvirt  # add each normal user to libvirt group
 done  
 
+declare -a arr_InputDeviceID=`ls /dev/input/by-id`  # list of input devices
+declare -a arr_InputDeviceEventID=`ls -l /dev/input/by-id | cut -d '/' -f2 | grep -v 'total 0'` # list of event IDs for input devices
+
 if [[ -z $arr_InputDeviceID ]]; then
     echo -e "$0: No input devices found. Skipping."
     exit 0
-else
-    declare -a arr_InputDeviceID=`ls /dev/input/by-id`  # list of input devices
-    declare -a arr_InputDeviceEventID=`ls -l /dev/input/by-id | cut -d '/' -f2 | grep -v 'total 0'` # list of event IDs for input devices
-    
+else    
     echo -en "$0: Executing Evdev setup... "
 
     # /etc/libvirt/qemu.conf #
     str_output1="user = \"$str_UID1000\"\ngroup = \"user\""
     str_output2=""
 
-    if [[ -e $str_file0 ]]; then
+    if [[ -e $str_logFile0 ]]; then
         str_output2="hugetlbfs_mount = \"/dev/hugepages\""
     fi
     
@@ -76,7 +78,7 @@ else
     str_output3+="    \"/dev/null\", \"/dev/full\", \"/dev/zero\",\n    \"/dev/random\", \"/dev/urandom\",\n    \"/dev/ptmx\", \"/dev/kvm\",\n    \"/dev/rtc\",\"/dev/hpet\"\n]"
 
     if [[ -e $str_inFile1 ]]; then
-        cp $str_outFile1 $str_oldFile2      # create backup
+        mv $str_outFile1 $str_oldFile2      # create backup
         cp $str_inFile1 $str_outFile1       # copy from template
 
         # write to file #
@@ -95,22 +97,12 @@ else
 
             echo -e $str_line1 >> $str_outFile1
         done < $str_inFile1
-
-        echo -e "Complete.\n"
-        systemctl enable libvirtd
-        systemctl restart libvirtd
-        echo -e "\n$0: Review changes:\n\t'$str_outFile1'"
-    else
-        echo -e "Failed. File(s) missing:"
-
-        if [[ -z $str_inFile1 ]]; then 
-            echo -e "\t'$str_inFile1'"
-        fi
-
-        if [[ -z $str_inFile2 ]]; then 
-            echo -e "\t'$str_inFile2'"
-        fi
     fi
+
+    echo -e "Complete."
+    systemctl enable libvirtd
+    systemctl restart libvirtd
+    echo -e "\n$0: Review changes:\n\t'$str_outFile1'"
 fi
 
 IFS=$SAVEIFS        # reset IFS     # NOTE: necessary for newline preservation in arrays and files
