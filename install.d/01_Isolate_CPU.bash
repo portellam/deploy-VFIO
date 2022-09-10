fabe39baf351ae7cfcbe717e2d9f992bf38edecc
portellam@gitkraken <name@example.com>
2022-09-10 00:06:20 -0400

    update

--- install.d/01_Isolate_CPU.bash
+++ install.d/01_Isolate_CPU.bash
@@ -8,7 +8,13 @@
 #
 #
 
-exit 0
+#
+# output:
+#
+# host threads, by pairs
+# virt threads, by pairs
+# complete arrays of each thread for either host and virt
+#
 
 # check if sudo/root #
     if [[ `whoami` != "root" ]]; then
@@ -26,95 +32,131 @@ exit 0
     readonly str_logFile1="$str_thisFile.log"
 
 # prompt #
-    str_output1="HugePages is a feature which statically allocates System Memory to pagefiles.\n\tVirtual machines can use HugePages to a peformance benefit.\n\tThe greater the Hugepage size, the less fragmentation of memory, and lower overhead of memory-access (memory latency).\n"
-    # echo -e $str_output1
-    echo -e "Executing CPU isolation setup..."
+    str_output="CPU isolation is a feature which statically allocates System CPU threads to the host and Virtual machines.\n\tVirtual machines can use CPU isolation or 'pinning' to a peformance benefit.\n\nExecuting CPU isolation setup..."
+    echo -e $str_output1
 
 # parameters #
-    readonly declare -i int_totalCores=`cat /proc/cpuinfo | grep 'cpu cores' | uniq`
-    readonly declare -i int_totalThreads=`cat /proc/cpuinfo | grep 'processor'`
-    readonly declare -i int_multiThread=$(( $int_totalThreads / $int_totalCores ))
-    readonly declare -i int_lastThread=$(( $int_totalThreads - 1 ))
-    readonly str_totalThreads="0-$int_lastThread"
-    declare -i int_hostCores=1                                                      # default value
+    readonly arr_coresByThread=(`cat /proc/cpuinfo | grep 'core id' | cut -d ':' -f2 | cut -d ' ' -f2`)
+    readonly int_totalCores=`cat /proc/cpuinfo | grep 'cpu cores' | uniq | cut -d ':' -f2 | cut -d ' ' -f2`
+    readonly int_totalThreads=`cat /proc/cpuinfo | grep 'siblings' | uniq | cut -d ':' -f2 | cut -d ' ' -f2`
+    readonly int_multiThread=$(( $int_totalThreads / $int_totalCores ))
+    # readonly int_lastThread=$(( $int_totalThreads - 1 ))
+    # readonly str_totalThreads="0-$int_lastThread"
+    declare -a arr_totalCores=()
+    # declare -a arr_hostCores=()
+    declare -a arr_hostThreads=()
+    # declare -a arr_virtCores=()
+    declare -a arr_virtThreads=()
+    declare -i int_hostCores=1          # default value
 
 # reserve remainder cores to host #
     # >= 4-core CPU, leave max 2
     if [[ $int_totalCores -ge 4 ]]; then
-        int_hostCores=2
+        readonly int_hostCores=2
 
     # 2 or 3-core CPU, leave max 1
     elif [[ $int_totalCores -le 3 && $int_totalCores -ge 2 ]]; then
-        int_hostCores=1
+        readonly int_hostCores=1
 
     # 1-core CPU, do not reserve cores to virt
     else
-        int_hostCores=$int_totalCores
+        readonly int_hostCores=$int_totalCores
     fi
 
-    # parse total cores #
+# group threads by core id #
     for (( int_i=0 ; int_i<$int_totalCores ; int_i++ )); do
 
-        # reserve cores to host #
-        if [[ $int_i -lt $int_hostCores ]]; then
-            arr_hostCores+=( $int_i )
+        # parameters #
+        str_line1=""
+        declare -i int_thisCore=${arr_coresByThread[int_i]}
 
-        # reserve cores to virt #
+        for (( int_count=0 ; int_count<$int_multiThread ; int_count++ )); do
+
+            # parameters #
+            int_thisThread=$((int_thisCore+int_totalCores*int_count))
+            str_line1+="$int_thisThread,"
+        done
+
+        # update parameters #
+        if [[ ${str_line1: -1} == "," ]]; then
+            str_line1=${str_line1::-1}
+        fi
+
+        arr_totalThreads+=("$str_line1")
+
+        # save output for cpu isolation (host) #
+        if [[ $int_thisCore -lt $int_hostCores ]]; then
+            arr_hostThreads+=("$str_line1")
+
+        # save output for cpuset/cpumask (qemu cpu pinning) #
         else
-            arr_virtCores+=( $int_i )
+            arr_virtThreads+=("$str_line1")
         fi
     done
 
-# save output to string for cpuset and cpumask #
-    # parse multi thread #
-    declare -i int_i=1
-    while [[ $int_i -le $int_multiThread ]]; do
+# update parameters #
+    readonly arr_totalThreads
+    readonly arr_hostThreads
+    readonly arr_virtThreads
 
-        # parse host cores #
-        for (( int_j=0 ; int_j<${arr_hostCores[@]} ; int_j++ )); do
-            declare -i int_k=$(( int_j++ ))
+    # if [[ ${str_hostThreads: -1} == "," ]]; then
+    #     readonly str_hostThreads=${str_hostThreads::-1}
+    # fi
 
-            # update array of host threads, for cpumask #
-            arr_hostThreads+=( $(( ${arr_hostCores[$int_j]} * $int_multiThread )) )
+    # if [[ ${str_virtThreads: -1} == "," ]]; then
+    #     readonly str_virtThreads=${str_virtThreads::-1}
+    # fi
 
-            # match next valid element, and diff is equal to 1, update temp array, for cpuset #
-            if [[ ! -z ${arr_hostCores[$int_k]} && $(( ${arr_hostCores[$int_k]} - ${arr_hostCores[$int_j]} )) -eq 1 ]]; then
-                declare -a arr_temp1=( ${arr_hostCores[0]}, ${arr_hostCores[$int_k]} )
-            fi
-        done
+# # save output to string for cpuset and cpumask #
+#     # parse multi thread #
+#     declare -i int_i=1
+#     while [[ $int_i -le $int_multiThread ]]; do
 
-        # parse host cores #
-        for (( int_j=0 ; int_j<${arr_virtCores[@]} ; int_j++ )); do
-            declare -i int_k=$(( int_j++ ))
+#         # parse host cores #
+#         for (( int_j=0 ; int_j<${arr_hostCores[@]} ; int_j++ )); do
+#             declare -i int_k=$(( int_j++ ))
 
-            # match next valid element, and diff is equal to 1, update temp array
-            if [[ ! -z ${arr_virtCores[$int_k]} && $(( ${arr_virtCores[$int_k]} - ${arr_virtCores[$int_j]} )) -eq 1 ]]; then
-                declare -a arr_temp2=( ${arr_virtCores[0]}, ${arr_virtCores[$int_k]} )
-            fi
-        done
+#             # update array of host threads, for cpumask #
+#             arr_hostCores+=( $(( ${arr_hostCores[$int_j]} * $int_multiThread )) )
 
-        # format and update list of host threads #
-        str_hostThreads+=$(( ${arr_temp1[0]} * $int_i ))"-"$(( ${arr_temp1[1]} * $int_i ))","
-        str_virtThreads+=$(( ${arr_temp2[0]} * $int_i ))"-"$(( ${arr_temp2[1]} * $int_i ))","
-        ((int_i++))
-    done
+#             # match next valid element, and diff is equal to 1, update temp array, for cpuset #
+#             if [[ ! -z ${arr_hostCores[$int_k]} && $(( ${arr_hostCores[$int_k]} - ${arr_hostCores[$int_j]} )) -eq 1 ]]; then
+#                 declare -a arr_temp1=( ${arr_hostCores[0]}, ${arr_hostCores[$int_k]} )
+#             fi
+#         done
 
-    # remove last separator #
-    if [[ ${str_hostThreads: -1} == "," ]]; then
-        str_hostThreads=${str_hostThreads::-1}
-    fi
+#         # parse host cores #
+#         for (( int_j=0 ; int_j<${arr_virtCores[@]} ; int_j++ )); do
+#             declare -i int_k=$(( int_j++ ))
 
-    if [[ ${str_virtThreads: -1} == "," ]]; then
-        str_virtThreads=${str_virtThreads::-1}
-    fi
+#             # match next valid element, and diff is equal to 1, update temp array
+#             if [[ ! -z ${arr_virtCores[$int_k]} && $(( ${arr_virtCores[$int_k]} - ${arr_virtCores[$int_j]} )) -eq 1 ]]; then
+#                 declare -a arr_temp2=( ${arr_virtCores[0]}, ${arr_virtCores[$int_k]} )
+#             fi
+#         done
 
-    str_output1=
+#         # format and update list of host threads #
+#         str_hostThreads+=$(( ${arr_temp1[0]} * $int_i ))"-"$(( ${arr_temp1[1]} * $int_i ))","
+#         str_virtThreads+=$(( ${arr_temp2[0]} * $int_i ))"-"$(( ${arr_temp2[1]} * $int_i ))","
+#         ((int_i++))
+#     done
+
+#     # update parameters #
+#     if [[ ${str_hostThreads: -1} == "," ]]; then
+#         readonly str_hostThreads=${str_hostThreads::-1}
+#     fi
+
+#     if [[ ${str_virtThreads: -1} == "," ]]; then
+#         readonly str_virtThreads=${str_virtThreads::-1}
+#     fi
+
+    # str_output1=""
 
     # find cpu mask #
     # readonly declare -i int_totalThreads_mask=$(( ( 2 ** $int_totalThreads ) - 1 ))
 
     # declare -i int_hostThreads_mask=0
-    # for int_element in ${arr_hostThreads[@]}; do
+    # for int_element in ${arr_hostCores[@]}; do
     #     int_hostThreads_mask+=$(( 2 ** $int_element ))
     # done
 
@@ -137,6 +179,34 @@ exit 0
     # 0-11      0b111111111111  FFF     # total cores
     # 0-1,6-7   0b000011000011  C3      # host cores
     #
+
+# debug prompt #
+    function DebugOutput
+    {
+        echo -e "$0: ========== DEBUG PROMPT ==========\n"
+
+        echo -e '${#arr_coresByThread[@]} == '"'${#arr_coresByThread[@]}'"
+        echo -e '${#arr_totalThreads[@]} == '"'${#arr_totalThreads[@]}'"
+        echo -e '$int_totalCores == '"'$int_totalCores'"
+        echo -e '$int_totalThreads == '"'$int_totalThreads'"
+        # echo -e '$int_totalThreads_mask == '"'$int_totalThreads_mask'"
+        echo -e '${#arr_hostThreads[@]} == '"'${#arr_hostThreads[@]}'"
+        echo -e '$int_hostCores == '"'$int_hostCores'"
+        # echo -e '$int_hostThreads_mask == '"'$int_hostThreads_mask'"
+        # echo -e '$int_lastThread == '"'$int_lastThread'"
+        echo -e '$int_multiThread == '"'$int_multiThread'"
+        echo -e '${#arr_virtThreads[@]} == '"'${#arr_virtThreads[@]}'"
+
+        for (( i=0 ; i<${#arr_coresByThread[@]} ; i++ )); do echo -e "$0: '$""{arr_coresByThread[$i]}'\t= '${arr_coresByThread[$i]}'"; done && echo
+        for (( i=0 ; i<${#arr_totalThreads[@]} ; i++ )); do echo -e "$0: '$""{arr_totalThreads[$i]}'\t= '${arr_totalThreads[$i]}'"; done && echo
+        for (( i=0 ; i<${#arr_hostThreads[@]} ; i++ )); do echo -e "$0: '$""{arr_hostThreads[$i]}'\t= '${arr_hostThreads[$i]}'"; done && echo
+        for (( i=0 ; i<${#arr_virtThreads[@]} ; i++ )); do echo -e "$0: '$""{arr_virtThreads[$i]}'\t= '${arr_virtThreads[$i]}'"; done && echo
+
+        echo -e "\n$0: ========== DEBUG PROMPT =========="
+        exit 0
+    }
+
+    DebugOutput     # uncomment to debug here
 
 # clear existing file #
     if [[ -e $str_logFile1 ]]; then
