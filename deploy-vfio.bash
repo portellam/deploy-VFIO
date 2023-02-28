7f0ab1df1d22858742e2548f32fc1244ab6795a8
Alex Portell <mail@example.com>
2023-02-27 22:53:49 -0500

    Reordered functions.

--- deploy-vfio.bash
+++ deploy-vfio.bash
@@ -22,14 +22,14 @@
     # <remarks> Extras </remarks>
     AddUserToGroups
     if $bool_alloc_cpu; then Allocate_CPU; fi
-    if $bool_hugepages; then Allocate_RAM $@; fi
-    if $bool_audio_loopback; then GuestAudioLoopback; fi
+    if $bool_hugepages; then Allocate_RAM; fi
     if $bool_evdev; then Virtual_KVM; fi
     Modify_QEMU
     if $bool_zram_swap; then RAM_Swapfile; fi
     if $bool_libvirt_hooks; then LibvirtHooks; fi
     if $bool_looking_glass; then VirtualVideoCapture; fi
     # if $bool_scream; then VirtualAudioCapture; fi
+    if $bool_audio_loopback; then GuestAudioLoopback; fi
 
     # <remarks> Main setup </remarks>
     case true in
