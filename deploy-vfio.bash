a3cf36939d069a5cef06e552be99460ae8913539
Alex Portell <mail@example.com>
2023-03-03 20:41:58 -0500

    Add boolean for null options.

--- deploy-vfio.bash
+++ deploy-vfio.bash
@@ -15,21 +15,23 @@
 # </code>
 
 # <code>
+    bool_full_setup=false
     IsSudoUser || exit $?
     if ! SetOptions $@; then GetUsage; exit $?; fi
-    if $bool_uninstall; then UninstallExisting_VFIO fi
+    if ! IsString $1; bool_full_setup=true
+    if ! $bool_full_setup && $bool_uninstall; then UninstallExisting_VFIO fi
 
     # <remarks> Extras </remarks>
     AddUserToGroups
-    if $bool_alloc_cpu; then Allocate_CPU; fi
-    if $bool_hugepages; then Allocate_RAM; fi
-    if $bool_evdev; then Virtual_KVM; fi
+    if $bool_full_setup || $bool_alloc_cpu; then Allocate_CPU; fi
+    if $bool_full_setup || $bool_hugepages; then Allocate_RAM; fi
+    if $bool_full_setup || $bool_evdev; then Virtual_KVM; fi
     Modify_QEMU
-    if $bool_zram_swap; then RAM_Swapfile; fi
-    if $bool_libvirt_hooks; then LibvirtHooks; fi
-    if $bool_looking_glass; then VirtualVideoCapture; fi
-    # if $bool_scream; then VirtualAudioCapture; fi
-    if $bool_audio_loopback; then GuestAudioLoopback; fi
+    if $bool_full_setup || $bool_zram_swap; then RAM_Swapfile; fi
+    if $bool_full_setup || $bool_libvirt_hooks; then LibvirtHooks; fi
+    if $bool_full_setup || $bool_looking_glass; then VirtualVideoCapture; fi
+    # if $bool_full_setup || $bool_scream; then VirtualAudioCapture; fi
+    if $bool_full_setup || $bool_audio_loopback; then GuestAudioLoopback; fi
 
     # <remarks> Main setup </remarks>
     case true in
@@ -48,22 +50,13 @@
 
     Select_IOMMU $@ || exit $?
 
-    case true in
-        $bool_multiboot )
-            Multiboot_VFIO
-            exit $?
-            ;;
-
-        $bool_static )
-            Static_VFIO
-            exit $?
-            ;;
-
-        * )
-            Setup_VFIO
-            exit $?
-            ;;
-    esac
+    if $bool_multiboot; then
+        Multiboot_VFIO || exit $?
+    elif $bool_static; then
+        Static_VFIO || exit $?
+    else
+        Setup_VFI0 || exit $?
+    fi
 # </code>
 
 exit 0
